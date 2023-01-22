fc9982c41f40883dc6b294ae1bcd1d417972016f
SupaSibs <omareeto2012@hotmail.com>
2023-01-23 01:33:10 +0300

    add empty

--- board.ts
+++ board.ts
@@ -1,5 +1,6 @@
 /* use strict */
 
+import { Pawn, Knight, Bishop, Rook, Queen, King } from "./classes";
 
 interface board {
   a: any[];
@@ -18,15 +19,94 @@ interface Position {
   centerSquares: any[];
 }
 
+class Empty {
+  position: string;
+  constructor(position: string) {
+    this.position = position;
+  }
+}
+
 const baseBoard: board = {
-  a: [],
-  b: [],
-  c: [],
-  d: [],
-  e: [],
-  f: [],
-  g: [],
-  h: [],
+  a: [
+    new Rook("a1", "white"),
+    new Pawn("a2", "white"),
+    new Empty("a3"),
+    new Empty("a4"),
+    new Empty("a5"),
+    new Empty("a6"),
+    new Pawn("a7", "black"),
+    new Rook("a8", "black"),
+  ],
+  b: [
+    new Knight("b1", "white"),
+    new Pawn("b2", "white"),
+    new Empty("b3"),
+    new Empty("b4"),
+    new Empty("b5"),
+    new Empty("b6"),
+    new Pawn("b7", "black"),
+    new Knight("b8", "black"),
+  ],
+  c: [
+    new Bishop("c1", "white"),
+    new Pawn("c2", "white"),
+    new Empty("c3"),
+    new Empty("c4"),
+    new Empty("c5"),
+    new Empty("c6"),
+    new Pawn("c7", "black"),
+    new Bishop("c8", "black"),
+  ],
+  d: [
+    new Queen("d1", "white"),
+    new Pawn("d2", "white"),
+    new Empty("d3"),
+    new Empty("d4"),
+    new Empty("d5"),
+    new Empty("d6"),
+    new Pawn("d7", "black"),
+    new Queen("d8", "black"),
+  ],
+  e: [
+    new King("e1", "white"),
+    new Pawn("e2", "white"),
+    new Empty("e3"),
+    new Empty("e4"),
+    new Empty("e5"),
+    new Empty("e6"),
+    new Pawn("e7", "black"),
+    new King("e8", "black"),
+  ],
+  f: [
+    new Bishop("f1", "white"),
+    new Pawn("f2", "white"),
+    new Empty("f3"),
+    new Empty("f4"),
+    new Empty("f5"),
+    new Empty("f6"),
+    new Pawn("f7", "black"),
+    new Bishop("f8", "black"),
+  ],
+  g: [
+    new Knight("g1", "white"),
+    new Pawn("g2", "white"),
+    new Empty("g3"),
+    new Empty("g4"),
+    new Empty("g5"),
+    new Empty("g6"),
+    new Pawn("g7", "black"),
+    new Knight("g8", "black"),
+  ],
+  h: [
+    new Rook("h1", "white"),
+    new Pawn("h2", "white"),
+    new Empty("h3"),
+    new Empty("h4"),
+    new Empty("h5"),
+    new Empty("h6"),
+    new Pawn("h7", "black"),
+    new Rook("h8", "black"),
+  ],
 };
 
 class Board implements Position {
--- classes.ts
+++ classes.ts
@@ -35,41 +35,16 @@ const alphabet = "abcdefgh";
 class Piece {
   position: string;
   moved: boolean;
-  constructor(position: string, moved = false) {
+  color: string;
+  constructor(position: string, color: string, moved = false) {
     this.position = position;
     this.moved = moved;
+    this.color = color;
   }
 }
-class Pawn extends Piece {
+export class Pawn extends Piece {
   validateMove(move: string) {
     //TODO: implement promotion
-    //d4 d3
-    /*if (
-      (move[2] != "x" &&
-        this.moved === true &&
-        parseInt(this.position[1]) + 1 == parseInt(move[1])) ||
-      (move[2] != "x" &&
-        this.moved === false &&
-        parseInt(this.position[1]) + 1 == parseInt(move[1])) ||
-      (move[2] != "x" &&
-        this.moved === false &&
-        parseInt(this.position[1]) + 2 == parseInt(move[1]))
-    ) {
-      return true;
-    }
-    if (
-      (move[2] === "x" &&
-        alphabet.indexOf(move[0]) + 1 ===
-          alphabet.indexOf(this.position[0]) + 2 &&
-        parseInt(move[1]) === parseInt(this.position[1]) + 1) ||
-      (move[2] === "x" &&
-        alphabet.indexOf(move[0]) + 1 === alphabet.indexOf(this.position[0]) &&
-        parseInt(move[1]) === parseInt(this.position[1]) + 1)
-    ) {
-      return true;
-    }
-    return false;*/
-
     const fromX = alphabet.indexOf(this.position[0]);
     const fromY = parseInt(this.position[1]);
     const toX = alphabet.indexOf(move[0]);
@@ -85,8 +60,7 @@ class Pawn extends Piece {
     );
   }
 }
-//pos 0-f 1-3 moves 1 0-g 1-3 2 0-e 1-1 3 0-g 1-5 4 0-e 1-5 5 0-d 1-4 6 0-d 1-2 7 0-h 1-4 8 0-h 1-2
-class Knight extends Piece {
+export class Knight extends Piece {
   validateMove(move: string) {
     const fromX = alphabet.indexOf(this.position[0]);
     const fromY = parseInt(this.position[1]);
@@ -99,8 +73,7 @@ class Knight extends Piece {
     return (distX == 2 && distY == 1) || (distX == 1 && distY == 2);
   }
 }
-
-class Bishop extends Piece {
+export class Bishop extends Piece {
   validateMove(move: string) {
     const fromX = alphabet.indexOf(this.position[0]);
     const fromY = parseInt(this.position[1]);
@@ -113,72 +86,51 @@ class Bishop extends Piece {
     return distX == distY && distX < 8 && distY < 8;
   }
 }
+export class Rook extends Piece {
+  validateMove(move: string) {
+    const fromX = alphabet.indexOf(this.position[0]);
+    const fromY = parseInt(this.position[1]);
+    const toX = alphabet.indexOf(move[0]);
+    const toY = parseInt(move[1]);
 
-class Rook extends Piece {
+    const distX = Math.abs(toX - fromX);
+    const distY = Math.abs(toY - fromY);
 
-validateMove(move: string) {
-	  const fromX = alphabet.indexOf(this.position[0]);
-	    const fromY = parseInt(this.position[1]);
-	      const toX = alphabet.indexOf(move[0]);
-	        const toY = parseInt(move[1]);
-
-		  const distX = Math.abs(toX - fromX);
-		    const distY = Math.abs(toY - fromY);
-
-		      return distX < 8 && distY == 0 || distX == 0 && distY < 8;
-}
+    return (distX < 8 && distY == 0) || (distX == 0 && distY < 8);
+  }
 }
 
-class Queen extends Piece {
+export class Queen extends Piece {
+  validateMove(move: string) {
+    const fromX = alphabet.indexOf(this.position[0]);
+    const fromY = parseInt(this.position[1]);
+    const toX = alphabet.indexOf(move[0]);
+    const toY = parseInt(move[1]);
 
-validateMove(move: string) {
-	  const fromX = alphabet.indexOf(this.position[0]);
-	    const fromY = parseInt(this.position[1]);
-	      const toX = alphabet.indexOf(move[0]);
-	        const toY = parseInt(move[1]);
+    const distX = Math.abs(toX - fromX);
+    const distY = Math.abs(toY - fromY);
 
-		  const distX = Math.abs(toX - fromX);
-		    const distY = Math.abs(toY - fromY);
-
-		      return distX = distY && distX < 8 && distY < 8 || distX == 0 && distY < 8 || distX < 8 && distY === 0
+    return (
+      (distX === distY && distX < 8 && distY < 8) ||
+      (distX == 0 && distY < 8) ||
+      (distX < 8 && distY === 0)
+    );
+  }
 }
+export class King extends Piece {
+  validateMove(move: string) {
+    const fromX = alphabet.indexOf(this.position[0]);
+    const fromY = parseInt(this.position[1]);
+    const toX = alphabet.indexOf(move[0]);
+    const toY = parseInt(move[1]);
+
+    const distX = Math.abs(toX - fromX);
+    const distY = Math.abs(toY - fromY);
+
+    return (
+      (distX == 1 && distY == 1) ||
+      (distX == 1 && distY == 0) ||
+      (distX == 0 && distY == 1)
+    );
+  }
 }
-
-class King extends Piece {
-
-validateMove(move: string) {
-	  const fromX = alphabet.indexOf(this.position[0]);
-	    const fromY = parseInt(this.position[1]);
-	      const toX = alphabet.indexOf(move[0]);
-	        const toY = parseInt(move[1]);
-
-		  const distX = Math.abs(toX - fromX);
-		    const distY = Math.abs(toY - fromY);
-
-		      return distX == 1 && distY == 1 || distX == 1 && distY == 0 || distX == 0 && distY == 1
-}
-
-}
-
-let x = new Pawn("e2");
-console.debug(
-  `${x.validateMove("e3")}  ${x.validateMove("e2")} ${x.validateMove(
-    "f3x"
-  )} ${x.validateMove("c3x")} ${x.validateMove("e4")}`
-);
-
-let y = new Knight("f3");
-console.debug(
-  `${y.validateMove("g1")} ${y.validateMove("e1")} ${y.validateMove(
-    "g5"
-  )} ${y.validateMove("e5")} ${y.validateMove("d4")} ${y.validateMove(
-    "d2"
-  )} ${y.validateMove("h4")} ${y.validateMove("h2")}`
-);
-
-let b = new Bishop("g2");
-console.debug(
-  `${b.validateMove("h1")} ${b.validateMove("h3")} ${b.validateMove(
-    "f3"
-  )} ${b.validateMove("f1")}`
-);
