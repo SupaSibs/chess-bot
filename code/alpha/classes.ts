19a60a3fe0b44b69ee5438921094cf19eef0626b
SupaSibs <92636078+SupaSibs@users.noreply.github.com>
2023-02-22 04:49:02 +0000

    sdfsdf

--- code/alpha/classes.ts
+++ code/alpha/classes.ts
@@ -31,6 +31,225 @@ const alphabet = "abcdefgh";
  * Queenside Castling is O-O-O
  *
  */
+
+class Piece {
+  position: string;
+  moved: boolean;
+  color: string;
+  constructor(position: string, color: string, moved = false) {
+    this.position = position;
+    this.moved = moved;
+    this.color = color;
+  }
+  get pos() {
+    return this.position;
+  }
+  get move() {
+    return this.moved;
+  }
+  get colour() {
+    return this.color;
+  }
+}
+export class Pawn extends Piece {
+  validateMove(move: string) {
+    //TODO: implement promotion
+    const fromX = alphabet.indexOf(this.position[0]);
+    const fromY = parseInt(this.position[1]);
+    const toX = alphabet.indexOf(move[0]);
+    const toY = parseInt(move[1]);
+
+    const distX = Math.abs(toX - fromX);
+    const distY = Math.abs(toY - fromY);
+
+    return (
+      (distX == 0 && distY == 1) ||
+      (move[2] == "x" && distX == 1 && distY == 1) ||
+      (this.moved === false && distX === 0 && distY == 2)
+    );
+  }
+}
+export class Knight extends Piece {
+  validateMove(move: string) {
+    const fromX = alphabet.indexOf(this.position[0]);
+    const fromY = parseInt(this.position[1]);
+    const toX = alphabet.indexOf(move[0]);
+    const toY = parseInt(move[1]);
+
+    const distX = Math.abs(toX - fromX);
+    const distY = Math.abs(toY - fromY);
+
+    return (distX == 2 && distY == 1) || (distX == 1 && distY == 2);
+  }
+}
+export class Bishop extends Piece {
+  validateMove(move: string) {
+    const fromX = alphabet.indexOf(this.position[0]);
+    const fromY = parseInt(this.position[1]);
+    const toX = alphabet.indexOf(move[0]);
+    const toY = parseInt(move[1]);
+
+    const distX = Math.abs(toX - fromX);
+    const distY = Math.abs(toY - fromY);
+
+    return distX == distY && distX < 8 && distY < 8;
+  }
+}
+export class Rook extends Piece {
+  validateMove(move: string) {
+    const fromX = alphabet.indexOf(this.position[0]);
+    const fromY = parseInt(this.position[1]);
+    const toX = alphabet.indexOf(move[0]);
+    const toY = parseInt(move[1]);
+
+    const distX = Math.abs(toX - fromX);
+    const distY = Math.abs(toY - fromY);
+
+    return (distX < 8 && distY == 0) || (distX == 0 && distY < 8);
+  }
+}
+
+export class Queen extends Piece {
+  validateMove(move: string) {
+    const fromX = alphabet.indexOf(this.position[0]);
+    const fromY = parseInt(this.position[1]);
+    const toX = alphabet.indexOf(move[0]);
+    const toY = parseInt(move[1]);
+
+    const distX = Math.abs(toX - fromX);
+    const distY = Math.abs(toY - fromY);
+
+    return (
+      (distX === distY && distX < 8 && distY < 8) ||
+      (distX == 0 && distY < 8) ||
+      (distX < 8 && distY === 0)
+    );
+  }
+}
+export class King extends Piece {
+  validateMove(move: string) {
+    const fromX = alphabet.indexOf(this.position[0]);
+    const fromY = parseInt(this.position[1]);
+    const toX = alphabet.indexOf(move[0]);
+    const toY = parseInt(move[1]);
+
+    const distX = Math.abs(toX - fromX);
+    const distY = Math.abs(toY - fromY);
+
+    return (
+      (distX == 1 && distY == 1) ||
+      (distX == 1 && distY == 0) ||
+      (distX == 0 && distY == 1)
+    );
+  }
+}
+interface board {
+  a: any[];
+  b: any[];
+  c: any[];
+  d: any[];
+  e: any[];
+  f: any[];
+  g: any[];
+  h: any[];
+}
+
+class Empty {
+  position: string;
+  constructor(position: string) {
+    this.position = position;
+  }
+}
+
+//Supporter end
+
+/* This Chess Engine uses an HCE, Hand-Picked Evaluation.
+ * NNUE shall be implemented later on.
+ */
+
+const baseBoard: board = {
+  a: [
+    new Rook("a1", "white"),
+    new Pawn("a2", "white"),
+    new Empty("a3"),
+    new Empty("a4"),
+    new Empty("a5"),
+    new Empty("a6"),
+    new Pawn("a7", "black"),
+    new Rook("a8", "black"),
+  ],
+  b: [
+    new Knight("b1", "white"),
+    new Pawn("b2", "white"),
+    new Empty("b3"),
+    new Empty("b4"),
+    new Empty("b5"),
+    new Empty("b6"),
+    new Pawn("b7", "black"),
+    new Knight("b8", "black"),
+  ],
+  c: [
+    new Bishop("c1", "white"),
+    new Pawn("c2", "white"),
+    new Empty("c3"),
+    new Empty("c4"),
+    new Empty("c5"),
+    new Empty("c6"),
+    new Pawn("c7", "black"),
+    new Bishop("c8", "black"),
+  ],
+  d: [
+    new Queen("d1", "white"),
+    new Pawn("d2", "white"),
+    new Empty("d3"),
+    new Empty("d4"),
+    new Empty("d5"),
+    new Empty("d6"),
+    new Pawn("d7", "black"),
+    new Queen("d8", "black"),
+  ],
+  e: [
+    new King("e1", "white"),
+    new Pawn("e2", "white"),
+    new Empty("e3"),
+    new Empty("e4"),
+    new Empty("e5"),
+    new Empty("e6"),
+    new Pawn("e7", "black"),
+    new King("e8", "black"),
+  ],
+  f: [
+    new Bishop("f1", "white"),
+    new Pawn("f2", "white"),
+    new Empty("f3"),
+    new Empty("f4"),
+    new Empty("f5"),
+    new Empty("f6"),
+    new Pawn("f7", "black"),
+    new Bishop("f8", "black"),
+  ],
+  g: [
+    new Knight("g1", "white"),
+    new Pawn("g2", "white"),
+    new Empty("g3"),
+    new Empty("g4"),
+    new Empty("g5"),
+    new Empty("g6"),
+    new Pawn("g7", "black"),
+    new Knight("g8", "black"),
+  ],
+  h: [
+    new Rook("h1", "white"),
+    new Pawn("h2", "white"),
+    new Empty("h3"),
+    new Empty("h4"),
+    new Empty("h5"),
+    new Empty("h6"),
+    new Pawn("h7", "black"),
+    new Rook("h8", "black"),
+  ],
+};
+
 interface Position {
   position: board;
   materialImbalance: number;
@@ -172,115 +391,4 @@ export class BoardBoiler implements Position {
     return pieceDetected;
   }
 
-}
-class Piece {
-  position: string;
-  moved: boolean;
-  color: string;
-  constructor(position: string, color: string, moved = false) {
-    this.position = position;
-    this.moved = moved;
-    this.color = color;
-  }
-  get pos() {
-    return this.position;
-  }
-  get move() {
-    return this.moved;
-  }
-  get colour() {
-    return this.color;
-  }
-}
-export class Pawn extends Piece {
-  validateMove(move: string) {
-    //TODO: implement promotion
-    const fromX = alphabet.indexOf(this.position[0]);
-    const fromY = parseInt(this.position[1]);
-    const toX = alphabet.indexOf(move[0]);
-    const toY = parseInt(move[1]);
-
-    const distX = Math.abs(toX - fromX);
-    const distY = Math.abs(toY - fromY);
-
-    return (
-      (distX == 0 && distY == 1) ||
-      (move[2] == "x" && distX == 1 && distY == 1) ||
-      (this.moved === false && distX === 0 && distY == 2)
-    );
-  }
-}
-export class Knight extends Piece {
-  validateMove(move: string) {
-    const fromX = alphabet.indexOf(this.position[0]);
-    const fromY = parseInt(this.position[1]);
-    const toX = alphabet.indexOf(move[0]);
-    const toY = parseInt(move[1]);
-
-    const distX = Math.abs(toX - fromX);
-    const distY = Math.abs(toY - fromY);
-
-    return (distX == 2 && distY == 1) || (distX == 1 && distY == 2);
-  }
-}
-export class Bishop extends Piece {
-  validateMove(move: string) {
-    const fromX = alphabet.indexOf(this.position[0]);
-    const fromY = parseInt(this.position[1]);
-    const toX = alphabet.indexOf(move[0]);
-    const toY = parseInt(move[1]);
-
-    const distX = Math.abs(toX - fromX);
-    const distY = Math.abs(toY - fromY);
-
-    return distX == distY && distX < 8 && distY < 8;
-  }
-}
-export class Rook extends Piece {
-  validateMove(move: string) {
-    const fromX = alphabet.indexOf(this.position[0]);
-    const fromY = parseInt(this.position[1]);
-    const toX = alphabet.indexOf(move[0]);
-    const toY = parseInt(move[1]);
-
-    const distX = Math.abs(toX - fromX);
-    const distY = Math.abs(toY - fromY);
-
-    return (distX < 8 && distY == 0) || (distX == 0 && distY < 8);
-  }
-}
-
-export class Queen extends Piece {
-  validateMove(move: string) {
-    const fromX = alphabet.indexOf(this.position[0]);
-    const fromY = parseInt(this.position[1]);
-    const toX = alphabet.indexOf(move[0]);
-    const toY = parseInt(move[1]);
-
-    const distX = Math.abs(toX - fromX);
-    const distY = Math.abs(toY - fromY);
-
-    return (
-      (distX === distY && distX < 8 && distY < 8) ||
-      (distX == 0 && distY < 8) ||
-      (distX < 8 && distY === 0)
-    );
-  }
-}
-export class King extends Piece {
-  validateMove(move: string) {
-    const fromX = alphabet.indexOf(this.position[0]);
-    const fromY = parseInt(this.position[1]);
-    const toX = alphabet.indexOf(move[0]);
-    const toY = parseInt(move[1]);
-
-    const distX = Math.abs(toX - fromX);
-    const distY = Math.abs(toY - fromY);
-
-    return (
-      (distX == 1 && distY == 1) ||
-      (distX == 1 && distY == 0) ||
-      (distX == 0 && distY == 1)
-    );
-  }
 }
